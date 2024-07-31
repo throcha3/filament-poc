25c965c2383919683ef10207490344022cc5863c
thiago rocha <thiago.rocha@aiqfome.com>
2024-07-31 14:54:26 -0300

    add fields in table and create form

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -23,7 +23,8 @@ class ProductResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('name'),
+                Forms\Components\TextInput::make('price'),
             ]);
     }
 
@@ -31,7 +32,8 @@ class ProductResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('price'),
             ])
             ->filters([
                 //
