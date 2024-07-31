0be186db68ec01e0e72cc22bcda4aca0a9211848
thiago rocha <thiago.rocha@aiqfome.com>
2024-07-31 16:23:22 -0300

    add Tag entity

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Product;
+use App\Models\Tag;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,10 +18,13 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'God User',
+            'email' => 'god@god.com',
+            'password' => bcrypt('123'),
         ]);
 
         Product::factory(3)->create();
+
+        Tag::factory(10)->create();
     }
 }
